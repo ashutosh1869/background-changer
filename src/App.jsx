95acd9301342d6234a67a67098a7769dfa6c355f
ashutosh1869 <127655895+ashutosh1869@users.noreply.github.com>
2024-09-19 01:58:13 +0530

    improved

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react'
 
 
 function App() {
-  const [color, setColor] = useState("green")
+  const [color, setColor] = useState("#ffa5ab")
 
   return (
     <div className="w-full h-screen flex flex-wrap justify-center text-white 
     " style={{backgroundColor: color}}>
       <div className="fixed bottom-4 flex gap-4 justify-center bg-white px-2 py-1 rounded-lg
        ">
-        <button className="bg-pink-300 px-2 py-1  outline-none rounded-lg" onClick={()=> setColor("#ffa5ab")}>PINK</button>
-        <button className="bg-gray-500 px-2 py-1  outline-none rounded-lg" onClick={()=> setColor("#c0c0c0")}>GREY</button>
+        <button className="bg-pink-300 px-2 py-1  outline-none rounded-full" onClick={()=> setColor("#ffa5ab")}>PINK</button>
+        <button className="bg-gray-500 px-2 py-1  outline-none rounded-full" onClick={()=> setColor("#c0c0c0")}>GREY</button>
 
-        <button className="bg-green-950 px-2 py-1  outline-none rounded-lg" onClick={()=> setColor("#52b788")}>GREEN</button>
-        <button className="bg-yellow-300 px-2 py-1  outline-none rounded-lg" onClick={()=> setColor("#f4d58d")}>YELLOW</button>
+        <button className="bg-green-300 px-2 py-1  outline-none rounded-full" onClick={()=> setColor("#52b788")}>GREEN</button>
+        <button className="bg-yellow-300 px-2 py-1  outline-none rounded-full" onClick={()=> setColor("#f4d58d")}>YELLOW</button>
        
 
       </div>
